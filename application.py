896012b46c4b7506736759ad24f2e73297dd7704
Peyton Sarmiento <peytonsarmiento@wireless-10-147-237-161.public.utexas.edu>
2017-03-22 15:33:36 -0500

    changed index routes

--- application.py
+++ application.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
-@application.route('/index', methods=['GET', 'POST'])
+@application.route('/')
 def index():
     return render_template('about.html')
 

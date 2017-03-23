ec6c5ae02bd76edfbc7a21d5f07d04d92bd9c5b6
Kieran Vanderslice <ktvslice@cs.utexas.edu>
2017-03-23 13:50:24 -0500

    Shell of test file made

---
+++ app/tests.py
@@ -0,0 +1,38 @@
+from io import StringIO
+from unittest import main, TestCase
+
+#
+# Do Function/File imports here once they exist
+#
+
+
+# -----------
+# TestNetflix
+# -----------
+
+
+class TestModels(TestCase):
+
+    # ---------
+    # Episodes
+    # ---------
+
+
+    # -------
+    # Houses
+    # -------
+
+
+    # -----------
+    # Characters
+    # -----------
+
+    return 1
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    main()

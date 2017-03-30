8ae2841e7b5303f2df4e23103bfe76b462cde000
Peyton Sarmiento <peytonsarmiento@wireless-10-145-19-76.public.utexas.edu>
2017-03-30 15:52:37 -0500

    fixed populate spacing and updated db with imagelinks

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 from app import app, db, models
+from populate import get_houses, get_characters
+
 
 if __name__ == "__main__":
+    chars = models.Character.query.all()
+    houses = models.House.query.all()
+    for c in chars:
+        db.session.delete(c)
+    for h in houses:
+        db.session.delete(h)
+    get_houses()
+    get_characters()
     app.run(debug=True)
 

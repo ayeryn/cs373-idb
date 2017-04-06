beb24e5ff7912b331a40a1a63051bfc8e5e2063c
Kieran Vanderslice <ktvslice@cs.utexas.edu>
2017-04-06 11:42:00 -0500

    Tests update

--- app/tests.py
+++ app/tests.py
@@ -1,5 +1,6 @@
 from io import StringIO
 from unittest import main, TestCase
+import models
 
 #
 # Do Function/File imports here once they exist
@@ -21,14 +22,14 @@ class TestModels(TestCase):
         test_episode = {"name": "TestEpisode", "season": "3", "previous_episode": "TestPrevEp",
                         "next_episode": "TestNextEp", "characters": "Jon Snow, Benjen Stark, Eddard Stark"}
         
-        ep = Episode(**test_episode)
+        ep = models.Episode(**test_episode)
         self.session.add(ep)
         result = self.session.query(Episode).filter(Episode.name == "TestEpisode").first()
 
         self.assertEqual(result.season, "3")
         self.assertEqual(result.next_episode, "TestNextEp")
-		self.assertEqual(result.previous_episode, "TestPrevEp")
-		self.assertEqual(result.characters, "Jon Snow, Benjen Stark, Eddard Stark")
+        self.assertEqual(result.previous_episode, "TestPrevEp")
+        self.assertEqual(result.characters, "Jon Snow, Benjen Stark, Eddard Stark")
 
 
 	def test_episode_delete(self):
@@ -49,19 +50,18 @@ class TestModels(TestCase):
     # Houses
     # -------
 
-	def test_house_insert(self):
-        test_house = {"name": "TestHouse", "region": "Norf", "words": "DA KING OF DA NORF",
-                        "current_lord": "TestLord", "heir": "TestHeir", "overlord" : "TestOverLord"}
-        
-        h = House(**test_house)
+    def test_house_insert(self):
+        test_house = {"name": "TestHouse", "region": "Norf", "words": "DA KING OF DA NORF", "current_lord": "TestLord", "heir": "TestHeir", "overlord": "TestOverLord"}
+
+        h = models.House(**test_house)
         self.session.add(h)
-		result = self.session.query(House).filter(House.name == "TestHouse").first()
+        result = self.session.query(House).filter(House.name == "TestHouse").first()
 
         self.assertEqual(result.region, "Norf")
         self.assertEqual(result.words, "DA KING OF DA NORF")
-		self.assertEqual(result.current_lord, "TestLord")
-		self.assertEqual(result.heir, "TestHeir")
-		self.assertEqual(result.overlord, "TestOverLord")
+        self.assertEqual(result.current_lord, "TestLord")
+        self.assertEqual(result.heir, "TestHeir")
+        self.assertEqual(result.overlord, "TestOverLord")
 
 
 	def test_house_delete(self):
@@ -88,17 +88,17 @@ class TestModels(TestCase):
                         "father": "OOP", "mother": "JavaScript", "spouse" : "Madam Tests.py",
                         "allegiances" : "House Java, House HTMl, House CSS", "played_by" : "Himself"}
         
-        c = Character(**test_character)
+        c = models.Character(**test_character)
         self.session.add(c)
-		result = self.session.query(Character).filter(Character.name == "Kieran").first()
+        result = self.session.query(Character).filter(Character.name == "Kieran").first()
 
         self.assertEqual(result.titles, "Lord of the Python Tests")
         self.assertEqual(result.aliases, "-")
-		self.assertEqual(result.father, "OOP")
-		self.assertEqual(result.mother, "JavaScript")
-		self.assertEqual(result.spouse, "Madam Tests.py")
-		self.assertEqual(result.allegiances, "House Java, House HTMl, House CSS")
-		self.assertEqual(result.played_by, "Himself")
+        self.assertEqual(result.father, "OOP")
+        self.assertEqual(result.mother, "JavaScript")
+        self.assertEqual(result.spouse, "Madam Tests.py")
+        self.assertEqual(result.allegiances, "House Java, House HTMl, House CSS")
+        self.assertEqual(result.played_by, "Himself")
 
 
 	def test_character_delete(self):

4165f33ecf4b32dbe439f52ced65ff3156e8cbd0
Peyton Sarmiento <peytonsarmiento@wireless-10-147-61-90.public.utexas.edu>
2017-04-06 21:17:37 -0500

    moved db

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import models, views
-from models import db
-
+db = SQLAlchemy()
 db.app = app
 db.init_app(app)
+
+from app import models, views
 
 

95d9d7c440c2c5f415fd5b791208bd413dcbb7d0
Apurva Gorti <apurvagorti@wireless-10-147-20-198.public.utexas.edu>
2017-04-06 21:12:36 -0500

    moved imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from models import db
-
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+from app import models, views
+from models import db
+
 db.app = app
 db.init_app(app)
 
 
-from app import models, views

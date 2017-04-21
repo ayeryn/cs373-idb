dc3555da0a83c989d708b42fd97455bf4d2f6e33
Apurva Gorti <apurvagorti@wireless-10-147-62-157.public.utexas.edu>
2017-04-20 20:45:56 -0500

    remove house duplicates

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from app import app as application, db, models
-from populate import get_alc
+from populate import get_houses
 
 if __name__ == "__main__":
-    # oldies = models.Episode.query.all()
+    # oldies = models.House.query.all()
     # for o in oldies:
     # 	db.session.delete(o)
     # db.session.commit()
+    # get_houses()
     application.run(debug=True)
 
